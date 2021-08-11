28601e940f16497c45c0bea0844f689c9ecb4fee
Muktar <mayunited12@yahoo.com>
2021-08-11 04:48:07 +0100

    vuetify config file

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+// import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: "mdi",
+  },
+  theme: {
+    options: { customProperties: true },
+    themes: {
+      light: {
+        toDark: "#262626",
+        primary: "#d9a51f",
+        secondary: "#19647E",
+        lightBG: "#f9f9f9",
+        error: "#b71c1c",
+      },
+    },
+  },
 });
