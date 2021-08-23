9ab0a752ee757f85b1ccac46106b0d0658c88268
Muktar <mayunited12@yahoo.com>
2021-08-23 08:21:03 +0100

    impl. social icon component

---
+++ src/components/socialIcons.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="d-flex">
+        <span v-for="(icon, index) in socials" :key="index">
+            <font-awesome-icon :icon="icon.icon" :color="color" class="mr-4" />
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        color: {
+            type: String,
+            default: "white"
+        }
+    },
+  data() {
+      return {
+          socials: [
+              {
+                  icon: { prefix: 'fab', iconName: 'facebook-square'},
+              },
+              {
+                  icon: { prefix: 'fab', iconName: 'twitter'},
+              },
+              {
+                  icon: { prefix: 'fab', iconName: 'instagram'},
+              },
+          ]
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
