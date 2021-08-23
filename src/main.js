d0018803a8db2e50f62ba6dc62a8a31aa0c8ed2b
Muktar <mayunited12@yahoo.com>
2021-08-23 08:18:46 +0100

    imp. font awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFacebookSquare, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+// library.add(faFacebook);
+library.add([faFacebookSquare, faTwitter, faInstagram]);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
