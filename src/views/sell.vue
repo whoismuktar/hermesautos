e5fe07c720279ae74d796363eba03259652a114b
Muktar <mayunited12@yahoo.com>
2021-08-25 00:57:52 +0100

    intr. sell page

---
+++ src/views/sell.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sell">
+    <page-hero :carImg="hero.carImg" :intro="hero.intro" :title="hero.title" :subtitle="hero.subtitle" :cta="hero.cta" :ctaAction="hero.ctaAction" />
+  </div>
+</template>
+
+<script>
+import pageHero from '../components/pageHero.vue'
+export default {
+    components: {
+        pageHero
+    },
+    data: () => ({
+      hero: {
+        carImg: require('../assets/img/gle-benz.png'),
+        intro: "FINANCE WITH Hermes Autos",
+        title: "Your Low Rate Awaits",
+        subtitle: "Apply in Minutes. Get Approved Fast. Highly Competitive Rates.",
+        cta: "Apply Now",
+        ctaAction: "",
+      },
+    })
+}
+</script>
+
+<style>
+
+</style>
