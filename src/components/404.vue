c9752eefa8d4293964fc48219a66325a9467531a
Muktar <mayunited12@yahoo.com>
2021-08-25 00:56:08 +0100

    intr. 404 page

---
+++ src/components/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="brands-page page 404 notfound">
+    <div
+      style="min-height: 50vh"
+      class="d-flex flex-column justify-center align-center"
+    >
+      <h2 class="text-center"><b>404</b></h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
