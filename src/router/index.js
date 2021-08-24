bc5160c478a96c3a025c5c1a332d2ce525a88c97
Muktar <mayunited12@yahoo.com>
2021-08-25 00:57:18 +0100

    intro. finance, 404 routes

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/finance',
+    name: 'Finance',
+    component: () => import('../views/finance.vue')
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import('../components/404.vue')
   }
 ]
 
