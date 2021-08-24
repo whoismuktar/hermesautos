240b2047d4f609c537445a2689fe1855f9cb06ba
Muktar <mayunited12@yahoo.com>
2021-08-25 00:56:22 +0100

    intro question component

---
+++ src/components/footerQuestion.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer-question mb-12">
+      <div class="text-center font-x2b mb-4">Questions?</div>
+
+      <v-row justify="center" class="text-updpercase">
+          <v-col cols="2">
+              <v-icon class="mr-2" color="primary">contact_support</v-icon><b>Visit Our Help Center</b>
+          </v-col>
+          <v-col cols="2">
+              <v-icon class="mr-2" color="primary">mail</v-icon><b>Send A Message</b>
+          </v-col>
+          <v-col cols="2">
+              <v-icon class="mr-2" color="primary">phone</v-icon><b>+123456789</b>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
