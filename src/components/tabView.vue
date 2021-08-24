7ddb15aad8584d8541845e7350e1ce42f2f7c627
Muktar <mayunited12@yahoo.com>
2021-08-25 00:56:58 +0100

    intr. tabview boilerplate

---
+++ src/components/tabView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tabs-view">
+      <v-row justify="center">
+          <v-col cols="3" v-for="(tab, index) in tabs" :key="index">
+              <div class="d-flex align-start">
+                  <v-icon color="primary" class="mr-3 mt-1">check_circle</v-icon>
+                  <div>
+                    <h2>{{ tab.title }}</h2>
+                    <div>{{ tab.content }}</div>
+                  </div>
+              </div>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tabs: {
+            type: Array,
+            defualt: ()=> []
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
