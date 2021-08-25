c1f5c67dd5af7b639f70f1d2780cfebf628ecc54
Muktar <mayunited12@yahoo.com>
2021-08-25 10:03:39 +0100

    vincard component

---
+++ src/components/vinCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="von-card">
+      <v-card flat tile :max-width="maxWidth" class="pa-8">
+        <v-card-title class="px-0">Check Your VIN</v-card-title>
+        <v-card-text class="px-0">
+        <div>Enter your VIN Number</div>
+        <v-text-field outlined dense hide-details="auto"></v-text-field>
+        </v-card-text>
+
+        <v-card-action>
+        <v-btn dark width="100%" height="50">Search</v-btn>
+        </v-card-action>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    maxWidth: {
+      type: String,
+      default: "100%"
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
