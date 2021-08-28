e4c7a40b214303e84cfd11b11959cb3437ceecc2
Muktar <mayunited12@yahoo.com>
2021-08-28 20:48:55 +0100

    intr. store locations component

---
+++ src/components/storeLocation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="storeLocation">
+      <div class="font-x2b text-center">
+        Visit our sell us your car locations
+    </div>
+      <v-row>
+          <v-col cols="4" class="borderTest">
+              <div class="d-flex">
+                <div>
+                    <v-img contain :src="require('../assets/img/pic.png')" class="mt-10" width="200"></v-img>
+                </div>
+
+                <div>
+                    <h2>Orlando, FL</h2>
+                    <div>Address</div>
+                    <div>Hours: 9 AM - 6 PM, Monday - Saturday </div>
+                    <div>Phone: (123) 222-3214</div>
+                </div>
+            </div>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
