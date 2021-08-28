72487323f1f98bb4c621999e86943da02adbd796
Muktar <mayunited12@yahoo.com>
2021-08-28 20:47:59 +0100

    import more fa icons

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import router from './router'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFacebookSquare, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEquals, faPiggyBank } from '@fortawesome/free-solid-svg-icons';
 
 // library.add(faFacebook);
-library.add([faFacebookSquare, faTwitter, faInstagram]);
+library.add([faFacebookSquare, faTwitter, faInstagram, faEquals, faPiggyBank]);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false
