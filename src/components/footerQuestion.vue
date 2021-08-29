6e9cdac0f98fcaaee51ce429cc5f96a70f2b1fa5
Muktar <mayunited12@yahoo.com>
2021-08-29 23:03:23 +0100

    fix alignment

--- src/components/footerQuestion.vue
+++ src/components/footerQuestion.vue
@@ -3,13 +3,13 @@
       <div class="text-center font-x2b mb-4">Questions?</div>
 
       <v-row justify="center" class="text-updpercase">
-          <v-col cols="2">
+          <v-col cols="2" class="text-center">
               <v-icon class="mr-2" color="primary">contact_support</v-icon><b>Visit Our Help Center</b>
           </v-col>
-          <v-col cols="2">
+          <v-col cols="2" class="text-center">
               <v-icon class="mr-2" color="primary">mail</v-icon><b>Send A Message</b>
           </v-col>
-          <v-col cols="2">
+          <v-col cols="2" class="text-center">
               <v-icon class="mr-2" color="primary">phone</v-icon><b>+123456789</b>
           </v-col>
       </v-row>
