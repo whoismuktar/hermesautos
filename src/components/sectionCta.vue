258519094a78cac6e436014ccfb851ec61c11d9c
Muktar <mayunited12@yahoo.com>
2021-09-01 00:49:54 +0100

    updated component

---
+++ src/components/sectionCta.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="career-cta text-center lightBG py-12">
+      <div class="font-x2b mb-5">{{ section.title }}</div>
+      <div class="mb-3">{{ section.subtitle }}</div>
+
+      <v-btn depressed color="primary">{{ section.cta }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    section: {
+      type: Object,
+      default: ()=> {}
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
