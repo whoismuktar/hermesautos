ae4acbda76710d50ab314a055a81222ce559ed30
Muktar <mayunited12@yahoo.com>
2021-12-03 21:57:55 +0100

    update content

--- src/components/footerQuestion.vue
+++ src/components/footerQuestion.vue
@@ -4,13 +4,24 @@
 
       <v-row justify="center" class="text-updpercase">
           <v-col cols="12" sm="2" class="text-center">
-              <v-icon class="mr-2" color="primary">contact_support</v-icon><b>Visit Our Help Center</b>
+              <div class="cursorMe" @click="$router.push('/contact')">
+                <v-icon class="mr-2 cursorMe" color="primary">contact_support</v-icon>
+                <b>Visit Our Help Center</b>
+              </div>
           </v-col>
           <v-col cols="12" sm="2" class="text-center">
-              <v-icon class="mr-2" color="primary">mail</v-icon><b>Send A Message</b>
+              <div class="cursorMe" @click="$router.push('/contact')">
+                  <v-icon class="mr-2 cursorMe" color="primary">mail</v-icon>
+                  <b>Send A Message</b>
+              </div>
           </v-col>
           <v-col cols="12" sm="2" class="text-center">
-              <v-icon class="mr-2" color="primary">phone</v-icon><b>+123456789</b>
+              <div class="cursorMe">
+                <v-icon class="mr-2 cursorMe" color="primary">phone</v-icon>
+                <b>
+                    <a href="tel:+13054248922">+1 305-424-8922</a>
+                </b>
+              </div>
           </v-col>
       </v-row>
   </div>
